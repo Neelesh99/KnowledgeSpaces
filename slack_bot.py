7ada1780b5d86dfed744fdc170e85ce2643c341d
Neelesh99 <neelesh.ravichandran1999@gmail.com>
2023-06-29 13:54:17 +0100

    Moved methods into main.py

--- slack_bot.py
+++ slack_bot.py
@@ -11,30 +11,28 @@ from construct_index import IndexMaker
 
 ssl_context = ssl.create_default_context(cafile=certifi.where())
 
-# Initializes your app with your bot token and socket mode handler
-client = WebClient(token=os.environ["SLACK_BOT_TOKEN"], ssl=ssl_context)
-app = App(client=client)
 
-@app.message("gpt index workspace")
-def index_workspace(message, say):
+def get_app():
+    client = WebClient(token=os.environ["SLACK_BOT_TOKEN"], ssl=ssl_context)
+    app = App(client=client)
 
-    list_channels = app.client.conversations_list().get("channels")
-    list_ids = []
-    for channel in list_channels:
-        if channel["is_member"]:
-            list_ids.append(channel["id"])
-    index = IndexMaker.get_index_from_slack(list_ids)
-    index.save_to_disk("workspace_index.json")
-    say("Workspace has been indexed: use query command to query it")
+    @app.message("gpt index workspace")
+    def index_workspace(message, say):
 
-@app.message("gpt query")
-def gpt_query(message, say):
-    split_on_query = str(message["text"]).split("gpt query")
-    actual_query = split_on_query[1]
-    index = GPTSimpleVectorIndex.load_from_disk('workspace_index.json')
-    response = index.query(actual_query)
-    say(response.response)
+        list_channels = app.client.conversations_list().get("channels")
+        list_ids = []
+        for channel in list_channels:
+            if channel["is_member"]:
+                list_ids.append(channel["id"])
+        index = IndexMaker.get_index_from_slack(list_ids)
+        index.save_to_disk("workspace_index.json")
+        say("Workspace has been indexed: use query command to query it")
 
-# Start your app
-if __name__ == "__main__":
-    SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
+    @app.message("gpt query")
+    def gpt_query(message, say):
+        split_on_query = str(message["text"]).split("gpt query")
+        actual_query = split_on_query[1]
+        index = GPTSimpleVectorIndex.load_from_disk('workspace_index.json')
+        response = index.query(actual_query)
+        say(response.response)
+    return app
