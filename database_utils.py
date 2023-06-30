b99dbf50447a86b157554ea6ffa7dab6e233d40a
Neelesh99 <neelesh.ravichandran1999@gmail.com>
2023-06-30 12:21:02 +0100

    Added pymongo based db getting function

--- database_utils.py
+++ database_utils.py
@@ -1,14 +1,18 @@
 import os
 
+from pymongo import MongoClient
+from pymongo.database import Database
+
 
 class DatabaseConfig:
 
-    def __init__(self, connection_string: str):
+    def __init__(self, connection_string: str, db_name: str):
         self.connection_string = connection_string
+        self.db_name = db_name
 
     def __eq__(self, other):
         if type(other) == DatabaseConfig:
-            return other.connection_string == self.connection_string
+            return other.connection_string == self.connection_string and other.db_name == self.db_name
         return False
 
     @staticmethod
@@ -16,6 +20,12 @@ class DatabaseConfig:
         connection_string_template = os.getenv("MONGO_CONNECTION_STRING_TEMPLATE")
         username = os.getenv("MONGO_USERNAME")
         password = os.getenv("MONGO_PASSWORD")
+        db_name = os.getenv("DB_NAME")
         username_replaced = connection_string_template.replace("<username>", username)
         full_string = username_replaced.replace("<password>", password)
-        return DatabaseConfig(full_string)
+        return DatabaseConfig(full_string, db_name)
+
+
+def get_db_from_config(config: DatabaseConfig) -> Database:
+    client = MongoClient(config.connection_string)
+    return client[config.db_name]
--- test_database_utils.py
+++ test_database_utils.py
@@ -2,7 +2,7 @@ import os
 import unittest
 from unittest import mock
 
-from database_utils import DatabaseConfig
+from database_utils import DatabaseConfig, get_db_from_config
 
 
 class DatabaseUtilsTestCase(unittest.TestCase):
@@ -10,9 +10,16 @@ class DatabaseUtilsTestCase(unittest.TestCase):
     @mock.patch.dict(os.environ, {
         "MONGO_CONNECTION_STRING_TEMPLATE": "mongodb+srv://<username>:<password>@cluster0.pxqerb3.mongodb.net/?retryWrites=true&w=majority",
         "MONGO_USERNAME": "userName",
-        "MONGO_PASSWORD": "somePassword"
+        "MONGO_PASSWORD": "somePassword",
+        "DB_NAME": "someDBName"
     })
     def test_will_get_database_config_from_env(self):
         database_config = DatabaseConfig.get_database_config_from_env()
-        expected_config = DatabaseConfig("mongodb+srv://userName:somePassword@cluster0.pxqerb3.mongodb.net/?retryWrites=true&w=majority")
+        expected_config = DatabaseConfig("mongodb+srv://userName:somePassword@cluster0.pxqerb3.mongodb.net/?retryWrites=true&w=majority", "someDBName")
         self.assertEqual(expected_config, database_config)
+
+    def test_will_get_database_client_from_config(self):
+        config = DatabaseConfig(
+            "mongodb+srv://userName:somePassword@cluster0.pxqerb3.mongodb.net/?retryWrites=true&w=majority", "someDBName")
+        db = get_db_from_config(config)
+        self.assertEqual(db.name, "someDBName")
