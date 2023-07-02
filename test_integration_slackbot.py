551031aee386943e4dad4a46f534af1f6a9875bd
Neelesh99 <neelesh.ravichandran1999@gmail.com>
2023-07-02 15:47:15 +0100

    adjusted tests for less flakyness

--- test_integration_slackbot.py
+++ test_integration_slackbot.py
@@ -18,6 +18,7 @@ class SlackBothIntegrationTestCase(unittest.TestCase):
 
     @pytest.mark.integration
     def test_will_index_workspace(self):
+        start = time.time()
         app_runner = get_app()
         SocketModeHandler(app_runner, os.environ["SLACK_APP_TOKEN"]).connect()
         time.sleep(10)
@@ -28,14 +29,12 @@ class SlackBothIntegrationTestCase(unittest.TestCase):
         list_channels = app.client.conversations_list().get("channels")
         list_ids = filter_channels(list_channels, ["test-channel"])
         app.client.chat_postMessage(channel=list_ids[0], text="gpt index workspace")
-        time.sleep(2)
-        start = time.time()
         for i in range(20):
             print("Waiting for response after " + str(i*10) + " seconds")
             time.sleep(10)
-            result = app.client.conversations_history(channel=list_ids[0], limit=1, oldest=str(start))
-            print(result)
+            result = app.client.conversations_history(channel=list_ids[0])
             messages = result["messages"]
+            print(messages[0])
             if len(messages) != 0:
                 if str(messages[0]["text"]).find("Workspace has been indexed") != -1:
                     return
@@ -43,6 +42,7 @@ class SlackBothIntegrationTestCase(unittest.TestCase):
 
     @pytest.mark.integration
     def test_will_query_workspace(self):
+        start = time.time()
         app_runner = get_app()
         SocketModeHandler(app_runner, os.environ["SLACK_APP_TOKEN"]).connect()
         time.sleep(10)
@@ -53,14 +53,12 @@ class SlackBothIntegrationTestCase(unittest.TestCase):
         list_channels = app.client.conversations_list().get("channels")
         list_ids = filter_channels(list_channels, ["test-channel"])
         app.client.chat_postMessage(channel=list_ids[0], text="gpt query workspace What colour is steve's car?")
-        time.sleep(1)
-        start = time.time()
         for i in range(20):
             print("Waiting for response after " + str(i * 10) + " seconds")
             time.sleep(10)
-            result = app.client.conversations_history(channel=list_ids[0], limit=1, oldest=str(start))
-            print(result)
+            result = app.client.conversations_history(channel=list_ids[0])
             messages = result["messages"]
+            print(messages[0])
             if len(messages) != 0:
                 if str(messages[0]["text"]).find("blue") != -1:
                     return
