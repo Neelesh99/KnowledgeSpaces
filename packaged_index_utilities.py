208a52d5b929f4a2f63ca4c10ff19f1db4ec615a
Neelesh99 <45288390+Neelesh99@users.noreply.github.com>
2023-07-10 13:47:48 +0100

    Added api server based querying

---
+++ packaged_index_utilities.py
@@ -0,0 +1,57 @@
+import json
+
+from llama_index import StorageContext, ServiceContext, LLMPredictor, load_index_from_storage
+
+from construct_index import get_local_llm_from_huggingface, get_model_config_from_env, IndexMaker, get_openai_api_llm
+from database_utils import DatabaseConfig, get_db_from_config
+from knowledge_space import KnowledgeSpace
+
+# Getting our app config from the environment
+model_config = get_model_config_from_env()
+
+
+def local_knowledge_space_model(knowledge_space: KnowledgeSpace):
+    index, service_context = full_index_local_knowledge_space_model(knowledge_space)
+    return index.as_query_engine(service_context=service_context)
+
+
+def full_index_local_knowledge_space_model(knowledge_space):
+    model = get_local_llm_from_huggingface(model_config)
+    literal_eval = json.loads(knowledge_space.index_string)
+    storage_context = StorageContext.from_dict(literal_eval)
+    service_context = ServiceContext.from_defaults(llm_predictor=LLMPredictor(llm=model),
+                                                   embed_model=IndexMaker.get_hf_embeddings())
+    index = load_index_from_storage(storage_context, service_context=service_context)
+    return index, service_context
+
+
+def open_ai_knowledge_space_model(knowledge_space: KnowledgeSpace):
+    index, service_context = full_index_open_ai_knowledge_space_model(knowledge_space)
+    return index.as_query_engine(service_context=service_context)
+
+
+def full_index_open_ai_knowledge_space_model(knowledge_space):
+    model = get_openai_api_llm(model_config)
+    storage_context = StorageContext.from_dict(json.loads(knowledge_space.index_string))
+    service_context = ServiceContext.from_defaults(llm_predictor=LLMPredictor(llm=model))
+    index = load_index_from_storage(storage_context, service_context=service_context)
+    return index, service_context
+
+
+# local_workspace_model gets a local LLM for the user (if LOCAL=True or not set in env variables)
+def local_workspace_model():
+    model = get_local_llm_from_huggingface(model_config)
+    storage_context = StorageContext.from_defaults(persist_dir="./storage")
+    service_context = ServiceContext.from_defaults(llm_predictor=LLMPredictor(llm=model),
+                                                   embed_model=IndexMaker.get_hf_embeddings())
+    index = load_index_from_storage(storage_context, service_context=service_context)
+    return index.as_query_engine(service_context=service_context)
+
+
+# open_ai_workspace_model gets an OpenAi API LLM for the user (if LOCAL=False in env variables)
+def open_ai_workspace_model():
+    model = get_openai_api_llm(model_config)
+    storage_context = StorageContext.from_defaults(persist_dir="./storage")
+    service_context = ServiceContext.from_defaults(llm_predictor=LLMPredictor(llm=model))
+    index = load_index_from_storage(storage_context, service_context=service_context)
+    return index.as_query_engine(service_context=service_context)
---
+++ testing_fast_api.py
@@ -0,0 +1,44 @@
+import json
+
+from fastapi import FastAPI
+from llama_index import StorageContext, ServiceContext, load_index_from_storage, LLMPredictor
+from pydantic import BaseModel
+
+from construct_index import get_model_config_from_env, get_local_llm_from_huggingface, IndexMaker, get_openai_api_llm
+from database_utils import DatabaseConfig, get_db_from_config, get_index
+from knowledge_space import KnowledgeSpace
+from packaged_index_utilities import local_knowledge_space_model, model_config, open_ai_knowledge_space_model, \
+    local_workspace_model, open_ai_workspace_model
+
+app = FastAPI()
+
+# Getting out database config from environment
+db_config = DatabaseConfig.get_database_config_from_env()
+
+# Getting db from db config
+db = get_db_from_config(db_config)
+
+# Getting knowledgespace collection
+knowledge_space_collection = db.get_collection("knowledge_space")
+
+class Query(BaseModel):
+    query: str
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.post("/query/knowledge_space/{knowledge_space}")
+async def query_knowledge_space(knowledge_space: str, query: Query):
+    knowledge_space = get_index(knowledge_space_collection, "test_server", knowledge_space)
+    try:
+        index = local_knowledge_space_model(
+            knowledge_space) if model_config.local else open_ai_knowledge_space_model(knowledge_space)
+    except:
+        index = local_workspace_model() if model_config.local else open_ai_workspace_model()
+    response = index.query(query.query)
+    return {"response": response.response}
+
+
+
