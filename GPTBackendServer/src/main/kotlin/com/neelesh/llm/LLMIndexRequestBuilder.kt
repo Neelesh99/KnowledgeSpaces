681f29e09d97059e21e47c534955692cbcf482b4
Neelesh99 <45288390+Neelesh99@users.noreply.github.com>
2023-07-23 16:23:33 +0100

    WIP LLMIndexRequestBuilder

---
+++ GPTBackendServer/src/main/kotlin/com/neelesh/llm/LLMIndexRequestBuilder.kt
@@ -0,0 +1,40 @@
+package com.neelesh.llm
+
+import com.neelesh.model.BlobReference
+import com.neelesh.model.IndexRequest
+import com.neelesh.model.UserDetails
+import org.http4k.core.ContentType
+import org.http4k.core.MultipartFormBody
+import org.http4k.lens.MultipartFormFile
+import java.io.InputStream
+
+class LLMIndexRequestBuilder {
+
+    companion object {
+        fun buildIndexRequest(
+            email: String,
+            knowledgeFileTarget: String,
+            blobs: List<Pair<BlobReference, InputStream>>
+        ) : MultipartFormBody {
+            val indexRequest = IndexRequest(
+                UserDetails(email),
+                knowledgeFileTarget,
+                blobs.map { it.first }
+            )
+
+            val listOfFiles = blobs.map { (reference, dataStream) ->
+                MultipartFormFile(reference.fileName, ContentType.OCTET_STREAM, dataStream)
+            }
+
+            val formBody =  MultipartFormBody()
+                .plus("indexRequestFileName" to "indexRequest.json")
+                .plus("indexRequest.json" to MultipartFormFile(
+                    "indexRequest.json",
+                    ContentType.OCTET_STREAM,
+                    indexRequest.toJson().toString().byteInputStream()
+                ))
+            return listOfFiles.fold(formBody) { body, file -> body.plus(file.filename to file) }
+        }
+    }
+
+}
---
+++ GPTBackendServer/src/test/kotlin/com/neelesh/llmInterface/LLMIndexRequestBuilderTest.kt
@@ -0,0 +1,54 @@
+package com.neelesh.llmInterface
+
+import com.neelesh.llm.LLMIndexRequestBuilder
+import com.neelesh.model.BlobReference
+import com.neelesh.model.DataType
+import com.neelesh.model.IndexRequest
+import com.neelesh.model.UserDetails
+import org.http4k.core.ContentType
+import org.http4k.core.MultipartEntity
+import org.http4k.core.MultipartFormBody
+import org.http4k.lens.MultipartFormFile
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import java.io.InputStream
+
+class LLMIndexRequestBuilderTest {
+
+    @Test
+    fun `will generate response from llm request builder`() {
+        val email = "someEmail"
+        val knowledgeFileTarget = "someFileName"
+        val byteInputStream = "someText".byteInputStream()
+        val blobs = listOf<Pair<BlobReference, InputStream>>(
+            BlobReference("someBlobId", DataType.PLAIN_TEXT, "someInfo.txt") to
+                    byteInputStream
+        )
+        val indexRequest = IndexRequest(UserDetails(email), knowledgeFileTarget, blobs.map { it.first })
+
+        val expectedForm = MultipartFormBody()
+            .plus("indexRequestFileName" to "indexRequest.json")
+            .plus("indexRequest.json" to MultipartFormFile(
+                "indexRequest.json",
+                ContentType.OCTET_STREAM,
+                indexRequest.toJson().toString().byteInputStream()
+            ))
+            .plus("someInfo.txt" to MultipartFormFile(
+                "someInfo.txt",
+                ContentType.OCTET_STREAM,
+                byteInputStream
+            ))
+
+        val actual = LLMIndexRequestBuilder.buildIndexRequest(email, knowledgeFileTarget, blobs)
+
+        Assertions.assertEquals(actual.field("indexRequestFileName"), expectedForm.field("indexRequestFileName"))
+        Assertions.assertEquals(String(actual.file("indexRequest.json")!!.content.readAllBytes()), String(expectedForm.file("indexRequest.json")!!.content.readAllBytes()))
+        val readAllBytes = actual.file("someInfo.txt")!!.content.readAllBytes()
+        val readAllBytes1 = expectedForm.file("someInfo.txt")!!.content.readAllBytes()
+        Assertions.assertEquals(readAllBytes, readAllBytes1)
+
+    }
+
+
+
+}
