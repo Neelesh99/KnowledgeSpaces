0b64bef652992ab5e7ce21a269c74401907385f3
Neelesh99 <45288390+Neelesh99@users.noreply.github.com>
2023-08-10 19:28:14 +0100

    Added update model

---
+++ frontend/frontend-app/src/model/UpdateModel.tsx
@@ -0,0 +1,13 @@
+export interface  SimpleKnowledgeFileUpdateRequest {
+    knowledgeFileId: string,
+    email: string,
+    newName: string | undefined,
+    newBlobs: string[] | undefined
+}
+
+export interface SimpleKnowledgeSpaceUpdateRequest {
+    knowledgeSpaceId: string,
+    email: string,
+    newName: string | undefined,
+    newFiles: string[] | undefined
+}
