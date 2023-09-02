9a25db5f587081115f03448494a2ad6fe04ef42f
Neelesh99 <45288390+Neelesh99@users.noreply.github.com>
2023-09-02 12:54:28 +0100

    Adding some info to the home page

--- frontend/frontend-app/src/components/NavigationButton.tsx
+++ frontend/frontend-app/src/components/NavigationButton.tsx
@@ -5,7 +5,7 @@ export const NavigationButton = ({ title, link }: { title: string, link: string 
     const location: Location = useLocation();
     console.log(location.pathname)
     const unselectedStyle = "flex w-32 h-12 bg-slate-800 rounded-sm items-center font-special text-lg cursor-pointer transition justify-center hover:bg-gradient-to-r from-emerald-500 to-sky-500"
-    const selectedStyle = "flex w-32 h-12 bg-slate-600 rounded-sm items-center font-special text-lg cursor-pointer transition justify-center hover:bg-gradient-to-r from-emerald-500 to-sky-500"
+    const selectedStyle = "flex w-32 h-12 bg-slate-600 rounded-t-sm items-center font-special text-lg cursor-pointer transition justify-center hover:bg-gradient-to-r from-emerald-500 to-sky-500"
 
     const navigate = useNavigate()
 
--- frontend/frontend-app/src/routes/Home.tsx
+++ frontend/frontend-app/src/routes/Home.tsx
@@ -17,9 +17,25 @@ export default function Home(props: HomeProps) {
                     </div>
                     <div
                         onClick={() => props.loginFunction()}
-                        className="flex w-full h-12 rounded-b-xl justify-center py-3 cursor-pointer transition duration-300 hover:bg-gradient-to-r from-sky-900 to-sky-500 hover:text-white">
+                        className="flex w-full h-12 rounded-b-xl justify-center py-3 cursor-pointer transition duration-300 hover:bg-gradient-to-r from-emerald-500 to-sky-500 hover:text-white">
                         Sign-up now
                     </div>
+                </div>
+            </div>
+        </div>
+        <div className="w-full flex h-fit bg-slate-100">
+            <div className="grid grid-cols-2 w-full">
+                <div className="flex p-4">
+                    <img className="rounded-xl" src={"https://static.bimago.pl/mediacache/catalog/product/cache/0/6/147660/image/1500x2240/1643cf240370c0352b784358b4070f9e/147660_1.jpg"}/>
+                </div>
+                <div className="p-4">
+                    <h1 className="font-special text-2xl">Knowledge files</h1>
+                    <p className="mt-4">
+                        We have oriented our service around "Knowledge Files", these are collections of separate
+                        pieces of data that you upload. All the data in any individual knowledge file is then sent to our
+                        LLM server which processes the data into a searchable space. You can then query these knowledge files
+                        in our Query page using natural language queries.
+                    </p>
                 </div>
             </div>
         </div>
