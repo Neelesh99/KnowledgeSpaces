a2c4cff03f6c07d207bb384031d01b79df10d4b7
Neelesh99 <45288390+Neelesh99@users.noreply.github.com>
2023-09-03 13:27:08 +0100

    Added spaces composition

--- frontend/frontend-app/src/routes/Compose.tsx
+++ frontend/frontend-app/src/routes/Compose.tsx
@@ -52,7 +52,7 @@ export default function Compose() {
     return <ChosenKnowledgeSpaceContext.Provider value={chosenSpace}>
         <div className="flex flex-col">
             <div className="w-full h-fit bg-slate-600">
-                <div className="text-3xl font-special p-3 text-white">Store</div>
+                <div className="text-3xl font-special p-3 text-white">Compose</div>
             </div>
             <div className="flex flex-col px-12 py-2 bg-slate-100">
                 <div className="grid grid-cols-2">
