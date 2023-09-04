31021267f975762394d39f498e072a82d24361a5
Neelesh99 <45288390+Neelesh99@users.noreply.github.com>
2023-09-04 17:16:52 +0100

    Added file uploading

--- frontend/frontend-app/src/components/StoreTabs.tsx
+++ frontend/frontend-app/src/components/StoreTabs.tsx
@@ -1,7 +1,12 @@
-import {useState} from 'react'
+import {ChangeEvent, useContext, useState} from 'react'
 import {Tab} from '@headlessui/react'
 import StoreType from "./StoreType";
 import StoreLink from "./StoreLink";
+import {SimpleBlobUploadRequest} from "../model/StorageModel";
+import {convertSimpleUploadRequestToForData, sendUploadForm} from "../service/StoreService";
+import {ChosenKnowledgeFileContext} from "../service/ChosenKnowledgeFileContext";
+import {AuthenticationContext} from "../service/AuthenticationContext";
+import {EnvironmentContext} from "../service/EnvironmentContext";
 
 function classNames(...classes: string[]) {
     return classes.filter(Boolean).join(' ')
@@ -16,8 +21,33 @@ export default function StoreTabs() {
         WebLink: [
         ],
     })
+    const targetFile = useContext(ChosenKnowledgeFileContext)
+    const user = useContext(AuthenticationContext)
+    const environment = useContext(EnvironmentContext)
 
     const [text, setText] = useState("")
+    const [file, setFile] = useState<File>()
+
+    function onFileUpload(event: ChangeEvent<HTMLInputElement>) {
+        console.log(event.target.files[0])
+        setFile(event.target.files[0])
+    }
+
+    function onFileSubmit() {
+        if (file === undefined) {
+            alert("No file selected")
+        } else {
+            const simpleUploadRequest: SimpleBlobUploadRequest = {
+                type: "PDF_DOCUMENT",
+                fileName: file.name,
+                dataStream: file,
+                knowledgeFileTarget: targetFile.id,
+                email: user.email
+            }
+            const formData = convertSimpleUploadRequestToForData(simpleUploadRequest)
+            void sendUploadForm(environment.backendPrefix, formData).then(r => console.log(r))
+        }
+    }
 
     return (
         <div className="w-full max-w-md px-4 py-16 sm:px-0">
@@ -62,9 +92,14 @@ export default function StoreTabs() {
                             'ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2'
                         )}
                     >
-                        <div className="h-full w-full flex items-center justify-center bg-slate-100 p-3 rounded-md hover:bg-slate-200 transition cursor-pointer" >
-                            Upload File
-                        </div>
+                        <input
+                            type="file"
+                            onChange={(e) => onFileUpload(e)}
+                            className="h-full w-full flex items-center justify-center bg-slate-100 p-3 rounded-md hover:bg-slate-200 transition cursor-pointer"/>
+                        <div
+                            className="h-full w-full flex items-center justify-center bg-slate-100 p-3 rounded-md hover:bg-slate-200 transition cursor-pointer"
+                            onClick={() => onFileSubmit()}
+                        >Upload File</div>
                     </Tab.Panel>
                     <Tab.Panel
                         key={2}
