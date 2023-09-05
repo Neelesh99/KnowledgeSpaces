b3492324296942be389bd33fa6a9adbe9a9a45f3
Neelesh99 <45288390+Neelesh99@users.noreply.github.com>
2023-09-05 17:31:35 +0100

    Setting up pdf reading for upload

--- LLMServer/api_check.py
+++ LLMServer/api_check.py
@@ -1,15 +1,17 @@
 import json
+from pathlib import Path
 
 import uvicorn
 from fastapi import Request, FastAPI
 from llama_index import Document, \
     StringIterableReader
+from llama_index.readers.file.docs_reader import PDFReader
 from pydantic import BaseModel
 
 from compose_graph import compose_graph_hf
 from construct_index import IndexMaker
 from database_utils import DatabaseConfig, get_db_from_config, save_index_api
-from index_request_handler import plain_text_handler
+from index_request_handler import plain_text_handler, save_upload_file_tmp
 from knowledge_space import KnowledgeFile, KnowledgeSpace
 from packaged_index_utilities import local_knowledge_space_model, full_index_local_knowledge_space_model
 
@@ -51,6 +53,10 @@ async def handle_index_request(request: Request):
                 data = form[blobReference["fileName"]]
                 text = plain_text_handler(await data.read())
                 documentsForIndex = documentsForIndex + StringIterableReader().load_data([text])
+            if blobReference["type"] == "PDF_DOCUMENT":
+                data = form[blobReference["fileName"]]
+                filePath = save_upload_file_tmp(data)
+                documentsForIndex = documentsForIndex + PDFReader().load_data(filePath)
         index = IndexMaker.get_hf_index_from_docs(documentsForIndex)
         save_index_api(index, indexRequestMap["userDetails"]["email"], indexRequestMap["knowledgeFileTarget"], knowledge_file_collection)
     return {"runId": "someRunId"}
@@ -68,6 +74,8 @@ async def handle_file_query(request: Request):
             knowledgeFileMap["blobIds"],
             knowledgeFileMap["indexDict"]
         )
+        print(knowledgeFile.id)
+        print(query)
         model = local_knowledge_space_model(knowledgeFile)
         return model.query(query).response
 
